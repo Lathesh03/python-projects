2756032f581b50649c27e3c36d9d832993c43f0c
Lathesh Neikar Krishnappa Harish <lathesh.neikar@gmail.com>
2025-08-06 21:37:29 -0400

    Update game.py

--- number-guessing-game/game.py
+++ number-guessing-game/game.py
@@ -4,7 +4,6 @@ secret_number = random.randint(1, 100)
 
 while True:
     try:
-        guess = int(input("Guess a number between 1 and 100: "))
         
         if guess < secret_number:
             print("Too low! Try again.")
