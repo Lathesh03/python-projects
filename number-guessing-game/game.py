a241af39968b9d2b288a249086455713fe711481
Lathesh Neikar Krishnappa Harish <lathesh.neikar@gmail.com>
2025-08-06 22:36:00 -0400

    Final version of the number guessing game, ready to play

--- number-guessing-game/game.py
+++ number-guessing-game/game.py
@@ -1,10 +1,12 @@
 import random
 secret_number = random.randint(1, 100)
+##print(f"The secret number is: {secret_number}") # Add this line
+# ... rest of your code
 # print(secret_number) # You can uncomment this line for testing
 
 while True:
     try:
-        
+        guess = int(input("Guess a number between 1 and 100:\n"))
         if guess < secret_number:
             print("Too low! Try again.")
         elif guess > secret_number:
